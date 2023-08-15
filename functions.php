c02d354c24be3ecd5cccba51ef71746317df69cd
Gill Guillermin <guillerg680@gmail.com>
2023-08-15 09:23:30 +0200

    creation du theme personnalise photographe-event

--- functions.php
+++ functions.php
@@ -10,4 +10,3 @@ add_action('wp_enqueue_scripts', 'theme_enqueue_scripts');
 function theme_enqueue_scripts() {
     wp_enqueue_script('theme-script', get_template_directory_uri() . '/js/script.js', array(), '1.0', true);
 } 
-
